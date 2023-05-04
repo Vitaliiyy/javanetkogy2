beb5674f3f4089f7daf8e4ad1defaafa0bf09c6f
Vitaliy <vetal.com92@gmail.com>
2023-05-04 22:17:15 +0600

    refresh

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int mainBalance = 100;
-        int summaPopolneniy = 1200;
-        int bonus = summaPopolneniy / 100;
-        int result = mainBalance + summaPopolneniy + bonus;
-        int result2 = mainBalance + summaPopolneniy;
-        if (summaPopolneniy > 1000) {
-            System.out.println(bonus + mainBalance + summaPopolneniy);
+        int balance = 100;
+        int income = 1100;
+        int bonus = income / 100;
+        int result = balance + income + bonus;
+        int resultWithoutBonus = balance + income; // результат без бонусов
+        if (income > 1000) {
+            System.out.println(result);
         } else {
-            System.out.println(result2);
+            System.out.println(resultWithoutBonus);
         }
 
 
